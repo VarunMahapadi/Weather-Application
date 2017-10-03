68f41592b35c1774b5d63af660b85e602a2da29a
VarunMahapadi <varun.mahapadi@nyu.edu>
2017-10-03 19:57:49 -0400

    Update helperService.js

--- helperService.js
+++ helperService.js
@@ -1,14 +1,14 @@
 app.service('helperService', function() {
     this.createUrl = function(lat, long, date) {
         if (date) {
-            var url = 'https://api.darksky.net/forecast/003f4be7bd3317ed869861ba784c5c15/' + lat + ',' + long + "," + date + 'T12:00:00';
+            var url = 'https://api.darksky.net/forecast/MY_KEY/' + lat + ',' + long + "," + date + 'T12:00:00';
             return url;
             /*
             Get UNIX time
             var unixtime = date.getTime();
-            return 'https://api.darksky.net/forecast/003f4be7bd3317ed869861ba784c5c15/' + lat + ',' + long + ',' + unixtime;*/
+            return 'https://api.darksky.net/forecast/MY_KEY/' + lat + ',' + long + ',' + unixtime;*/
         } else {
-            return 'https://api.darksky.net/forecast/003f4be7bd3317ed869861ba784c5c15/' + lat + ',' + long;
+            return 'https://api.darksky.net/forecast/MY_KEY/' + lat + ',' + long;
         }
     }
 });
