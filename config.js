e5789fd88cd4185db680aadce0e197aa8129c347
VarunMahapadi <varun.mahapadi@nyu.edu>
2017-10-03 19:56:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    apiKey: "AIzaSyDHv-CoECbzIeZV9Y12-ZRUs2HIFKIc72k",
+    apiKey: "ENTER THE KEY",
     authDomain: "weatherapp-c8a09.firebaseapp.com",
     databaseURL: "https://weatherapp-c8a09.firebaseio.com",
     projectId: "weatherapp-c8a09",
     storageBucket: "weatherapp-c8a09.appspot.com",
-    messagingSenderId: "765689649161"
+    messagingSenderId: "MY ID"
 };
 firebase.initializeApp(config);
